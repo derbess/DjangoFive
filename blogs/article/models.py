18c745858853645bb2ade192be1360e610471ad4
Derbes Utebaliyev <uderbes@gmail.com>
2022-01-29 13:53:31 +0600

    create article

--- blogs/article/models.py
+++ blogs/article/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from datetime import date
+from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -10,3 +13,12 @@ class Category(models.Model):
 
     class Meta:
         verbose_name_plural = "Categories"
+
+class Article(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    category_id = models.ForeignKey(Category, on_delete=models.CASCADE)
+    creation_date = models.DateField(default=timezone.now)
+    image = models.TextField(default=None)
+    author_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    view = models.IntegerField(default=0)
